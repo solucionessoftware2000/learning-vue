af8b91e943a717af5716d5cf95438463b00b5949
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2025-04-23 15:38:45 -0500

    Breve detalle de Option API

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import PostContainer from './components/PostContainer.vue';
+import CounterOption from './components/CounterOption.vue';
 </script>
 
 <template>
-  <h1 class="post-head">Listado de posts:</h1>
-  <PostContainer/>
+  <CounterOption/>
 </template>
 
 <style scoped>
-.post-head{
-  color: white;
-}
 </style>
---
+++ src/components/CounterOption.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'CounterOption',
+    components: {},
+    data(){
+        let counter: number = 0;
+        return {counter}
+    },
+    methods:{
+        addCounter(){
+            this.counter++
+        }
+    }
+})
+</script>
+
+<template>
+    <h3>{{ counter }}</h3>
+    <button @click="addCounter">Incrementar</button>
+</template>
+
+<style scoped>
+</style>
