f04856f2761083e0390c8b2076e4960b20084e8d
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2025-04-23 15:21:53 -0500

    Creando un componente

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@ import PostContainer from './components/PostContainer.vue';
 </script>
 
 <template>
+  <h1 class="post-head">Listado de posts:</h1>
   <PostContainer/>
 </template>
 
 <style scoped>
+.post-head{
+  color: white;
+}
 </style>
--- src/components/PostContainer.vue
+++ src/components/PostContainer.vue
@@ -3,7 +3,6 @@ import PostCard from './PostCard.vue';
 </script>
 
 <template>
-    <h1 class="post-head">Listado de posts:</h1>
     <div class="post-container">
         <PostCard/>
         <PostCard/>
@@ -13,7 +12,9 @@ import PostCard from './PostCard.vue';
 </template>
 
 <style scoped>
-.post-head{
-    color: white;
+.post-container{
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
 }
 </style>
