a78eeb0a18daa930c806b5bacdc609d5228d22ee
solucionessoftware2000 <soluciones.software.2000@gmail.com>
2025-04-24 20:32:25 -0500

    Directiva for ejemplo2

--- src/components/ExampleFor.vue
+++ src/components/ExampleFor.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 import { ref, type Ref } from 'vue';
 
-const countries:Ref<Array<string>> = ref([
-    'Spain', 'France', 'UK', 'Italy', 'Portugal'
+interface ICountry {
+    code: string,
+    name: string
+}
+
+const countries:Ref<Array<ICountry>> = ref([
+    {
+        code: 'es',
+        name: 'Spain'
+    },
+    {
+        code: 'en',
+        name: 'UK'
+    },
+    {
+        code: 'fr',
+        name: 'France'
+    }
 ])
 </script>
 
 <template>
     <ul>
-        <li v-for="country in countries" :key="country">{{ country }}</li>
+        <li v-for="country in countries" :key="country.code">{{ country.name }}</li>
     </ul>
 </template>
 
